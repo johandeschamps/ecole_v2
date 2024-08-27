4e8abd290bfcbe3488361c3937260f6fbc6c073d
Johan Deschamps <johan.deschamps@fms-ea.com>
2024-08-27 08:39:29 +0200

    add business files

---
+++ business/school.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+"""
+Classe School
+"""
+
+from dataclasses import dataclass, field
+from models.course import Course
+from models.teacher import Teacher
+from models.student import Student
+
+
+@dataclass
+class School:
+    """Couche métier de l'application de gestion d'une école,
+    reprenant les cas d'utilisation et les spécifications fonctionnelles :
+    - courses : liste des cours existants
+    - teachers : liste des enseignants
+    - students : liste des élèves"""
+
+    courses: list[Course] = field(default_factory=list, init=False)
+    teachers: list[Teacher] = field(default_factory=list, init=False)
+    students: list[Student] = field(default_factory=list, init=False)
+
+    def add_course(self, course: Course) -> None:
+        """Ajout du cours course à la liste des cours."""
+        self.courses.append(course)
+
+    def add_teacher(self, teacher: Teacher) -> None:
+        """Ajout de l'enseignant teacher à la liste des enseignants."""
+        self.teachers.append(teacher)
+
+    def add_student(self, student: Student) -> None:
+        """Ajout de l'élève spécifié à la liste des élèves."""
+        self.students.append(student)
+
+    def display_courses_list(self) -> None:
+        """Affichage de la liste des cours avec pour chacun d'eux :
+        - leur enseignant
+        - la liste des élèves le suivant"""
+        for course in self.courses:
+            print(f"cours de {course}")
+            for student in course.students_taking_it:
+                print(f"- {student}")
+            print()
